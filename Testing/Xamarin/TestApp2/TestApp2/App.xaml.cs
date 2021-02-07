69c27c43058f289dd2391e6257ca7e0c1c997f88
Thomas Kreidl <vanthomiy@live.de>
2021-02-07 15:19:10 +0100

    Test App 2 Setup

---
+++ Testing/Xamarin/TestApp2/TestApp2/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TestApp2.Services;
+using TestApp2.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestApp2
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Testing/Xamarin/TestApp2/TestApp2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TestApp2.ViewModels;
+using Xamarin.Forms;
+
+namespace TestApp2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
