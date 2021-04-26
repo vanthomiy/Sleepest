b4e9f7c279f9a5cc045aedac41d872451d635b26
Thomas Kreidl <vanthomiy@live.de>
2021-04-26 22:07:31 +0200

    Some tutorials

--- Algorithm/MLModel/CreateModel.py
+++ Algorithm/MLModel/CreateModel.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # Convert the model
 
-dir = 'E:\\Dokumente\\Programmieren\\Projekte\\Sleepest\\Algorithm\\MLModel\\models\\Google\\tf_saved_model-combined04_20210426111459-2021-04-26T12_32_21.913276Z'
+dir = 'E:\\Dokumente\\Programmieren\\Projekte\\Sleepest\\Algorithm\\MLModel\\my_pet_classifier'
 converter = tf.lite.TFLiteConverter.from_saved_model(dir) # path to the SavedModel directory
 tflite_model = converter.convert()
 
