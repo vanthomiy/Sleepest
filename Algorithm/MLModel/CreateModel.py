b81daf34c430394600d3f1338cf966d4bac46686
Thomas Kreidl <vanthomiy@live.de>
2021-04-26 18:32:22 +0200

    Added google model

--- Algorithm/MLModel/CreateModel.py
+++ Algorithm/MLModel/CreateModel.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 
 # Convert the model
-converter = tf.lite.TFLiteConverter.from_saved_model('model') # path to the SavedModel directory
+
+dir = 'E:\\Dokumente\\Programmieren\\Projekte\\Sleepest\\Algorithm\\MLModel\\models\\Google\\tf_saved_model-combined04_20210426111459-2021-04-26T12_32_21.913276Z'
+converter = tf.lite.TFLiteConverter.from_saved_model(dir) # path to the SavedModel directory
 tflite_model = converter.convert()
 
 # Save the model.
---
+++ Algorithm/MLModel/CustomModel/TensorFlow.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+
+#---
+
+import pathlib
+
+dataset_url = 'http://storage.googleapis.com/download.tensorflow.org/data/petfinder-mini.zip'
+csv_file = 'datasets/petfinder-mini/petfinder-mini.csv'
+
+tf.keras.utils.get_file('petfinder_mini.zip', dataset_url,
+                        extract=True, cache_dir='.')
+dataframe = pd.read_csv(csv_file)
+
+#----
+
+# In the original dataset "4" indicates the pet was not adopted.
+dataframe['target'] = np.where(dataframe['AdoptionSpeed']==4, 0, 1)
+
+# Drop un-used columns.
+dataframe = dataframe.drop(columns=['AdoptionSpeed', 'Description'])
+
+#---
+
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
