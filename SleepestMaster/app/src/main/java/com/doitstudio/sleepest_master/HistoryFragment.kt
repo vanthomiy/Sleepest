47704b3029df366a8eaa37baa3de3124a1588f4f
Meraxa <60232880+Meraxa@users.noreply.github.com>
2021-05-16 17:30:54 +0200

    Added line chart

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/HistoryFragment.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/HistoryFragment.kt
@@ -6,22 +6,63 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.appyvet.rangebar.Bar
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.*
 
 class HistoryFragment : Fragment() {
-    private val barChart = view?.findViewById<View>(R.id.barChart) as BarChart
+    private lateinit var barChart: BarChart
+    private lateinit var lineChart: LineChart
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        barChart
-        setBarChart()
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        barChart = view.findViewById(R.id.barChart)
+        lineChart = view.findViewById(R.id.lineChart)
+
+        setBarChart()
+        setLineChart()
+    }
+
+    private fun setLineChart() {
+        val entries = ArrayList<Entry>()
+
+        entries.add(Entry(1f, 0))
+        entries.add(Entry(2f, 1))
+        entries.add(Entry(3f, 2))
+        entries.add(Entry(4f, 3))
+        entries.add(Entry(5f, 4))
+
+        val vl = LineDataSet(entries, "My Type")
+        vl.setDrawValues(false)
+        vl.setDrawFilled(true)
+        vl.lineWidth = 3f
+        vl.fillColor = R.color.gray
+        vl.fillAlpha = R.color.red
+
+        lineChart.xAxis.labelRotationAngle = 0f
+
+        lineChart.data = LineData()
+
+        lineChart.axisRight.isEnabled = false
+
+        lineChart.xAxis.mAxisMaximum = 0.1f
+
+        lineChart.setTouchEnabled(true)
+        lineChart.setPinchZoom(true)
+
+        lineChart.contentDescription = "Days"
+        lineChart.setNoDataText("No forex yet!")
+
+        lineChart.animateX(1800)
     }
 
     private fun setBarChart() {
@@ -42,12 +83,12 @@ class HistoryFragment : Fragment() {
         labels.add("21-Jan")
         labels.add("22-Jan")
         labels.add("23-Jan")
+
         val data = BarData(labels, barDataSet)
         barChart.data = data // set the data and list of lables into chart
 
         barChart.setDescription("Set Bar Chart Description")  // set the description
 
-        //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
         barDataSet.color = resources.getColor(R.color.colorAccent)
 
         barChart.animateY(5000)
