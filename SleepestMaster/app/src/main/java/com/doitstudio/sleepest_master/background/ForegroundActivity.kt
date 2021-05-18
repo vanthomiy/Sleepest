64b1969d2e20715a885eacbbfc5aee8eca065a92
Thomas Kreidl <vanthomiy@live.de>
2021-05-18 08:44:26 +0200

    Foreground: Check if a alarm is active

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/background/ForegroundActivity.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/background/ForegroundActivity.kt
@@ -2,10 +2,22 @@ package com.doitstudio.sleepest_master.background
 
 import android.app.Activity
 import android.os.Bundle
+import com.doitstudio.sleepest_master.MainApplication
 import com.doitstudio.sleepest_master.R
 import com.doitstudio.sleepest_master.model.data.Actions
+import com.doitstudio.sleepest_master.storage.DbRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class ForegroundActivity : Activity() {
+
+    private val scope: CoroutineScope = MainScope()
+    private val dbRepository: DbRepository by lazy {
+        (applicationContext as MainApplication).dbRepository
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_foreground)
@@ -18,7 +30,17 @@ class ForegroundActivity : Activity() {
 
                 /**TODO: Abfrage, ob Alarm da is. Wenn ja, startForeground, wenn nicht, AlarmReceiver für 20:00 nächster Tag einstellen*/
 
-                ForegroundService.startOrStopForegroundService(Actions.START, applicationContext)
+                // get the next alarm async
+                scope.launch {
+
+                    // next alarm or null
+                    val alarm = dbRepository.getNextActiveAlarm()
+
+                    if(alarm != null){
+                        // start foreground if not null
+                        ForegroundService.startOrStopForegroundService(Actions.START, applicationContext)
+                    }
+                }
             } else if (use == 2) {
                 ForegroundService.startOrStopForegroundService(Actions.STOP, applicationContext)
             }
