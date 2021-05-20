d66468e633a8f093fb30dca8e14211c7921420d6
Thomas Kreidl <vanthomiy@live.de>
2021-05-20 15:01:28 +0200

    Test database entries of sleepdatas

--- SleepestMaster/app/src/androidTest/java/com/doitstudio/sleepest_master/storage/DbRepositoryTest.kt
+++ SleepestMaster/app/src/androidTest/java/com/doitstudio/sleepest_master/storage/DbRepositoryTest.kt
@@ -7,14 +7,23 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.asLiveData
 import androidx.test.platform.app.InstrumentationRegistry
+import com.doitstudio.sleepest_master.model.data.SleepState
+import com.doitstudio.sleepest_master.sleepcalculation.SleepCalculationDbRepository
+import com.doitstudio.sleepest_master.sleepcalculation.db.SleepCalculationDatabase
+import com.doitstudio.sleepest_master.sleepcalculation.db.UserSleepSessionEntity
+import com.doitstudio.sleepest_master.sleepcalculation.model.algorithm.SleepTimes
 import com.doitstudio.sleepest_master.storage.db.AlarmEntity
+import com.doitstudio.sleepest_master.storage.db.SleepApiRawDataEntity
+import com.doitstudio.sleepest_master.storage.db.SleepApiRawDataRealEntity
 import com.doitstudio.sleepest_master.storage.db.SleepDatabase
+import com.google.gson.Gson
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import java.io.BufferedReader
 import java.time.DayOfWeek
 import java.time.LocalDate
 
@@ -22,11 +31,16 @@ class DbRepositoryTest {
 
     private lateinit var context: Context
     private lateinit var sleepDbRepository: DbRepository
+    private lateinit var sleep1DbRepository: SleepCalculationDbRepository
 
     private val alarmLivedata by lazy { sleepDbRepository.activeAlarmsFlow().asLiveData() }
 
     private val dbDatabase by lazy {
         SleepDatabase.getDatabase(context)
+    }
+
+    private val db1Database by lazy {
+        SleepCalculationDatabase.getDatabase(context)
     }
 
 
@@ -39,6 +53,10 @@ class DbRepositoryTest {
             dbDatabase.sleepDataDao(),
             dbDatabase.userSleepSessionDao(),
             dbDatabase.alarmDao()
+        )
+
+        sleep1DbRepository = SleepCalculationDbRepository.getRepo(
+                db1Database.sleepApiRawDataDao()
         )
     }
 
@@ -111,6 +129,68 @@ class DbRepositoryTest {
         alarms = sleepDbRepository.activeAlarmsFlow().first()
         assertThat(alarms.count(), CoreMatchers.equalTo(1))
 
+    }
+
+    @Test
+    fun insertSleepData() = runBlocking {
+
+        sleepDbRepository.deleteSleepSegments()
+        sleep1DbRepository.deleteSleepApiRawData()
+
+        // load all data
+        var pathTrue = "databases/testdata/SleepValuesTrue.json"
+
+        var gson = Gson()
+
+        val jsonFileTrue = context
+                .assets
+                .open(pathTrue)
+                .bufferedReader()
+                .use(BufferedReader::readText)
+
+        var dataTrue =  gson.fromJson(jsonFileTrue, Array<Array<SleepApiRawDataRealEntity>>::class.java).asList()
+
+
+
+        for(i in 0..10){
+
+
+            var startTime = dataTrue[i].filter { x -> x.real != "awake" }.minByOrNull { y -> y.timestampSeconds }!!.timestampSeconds
+            var endTimeTime = dataTrue[i].filter { x -> x.real != "awake" }.maxByOrNull { y -> y.timestampSeconds }!!.timestampSeconds
+
+            dataTrue[i].forEach{
+                singledata->
+
+                var sleepstate = SleepState.NONE
+
+                if(singledata.real == "awake"){
+                    sleepstate = SleepState.AWAKE
+                } else if(singledata.real == "sleeping"){
+                    sleepstate = SleepState.SLEEPING
+                } else if(singledata.real == "deep"){
+                    sleepstate = SleepState.DEEP
+                } else if(singledata.real == "light"){
+                    sleepstate = SleepState.LIGHT
+                } else if(singledata.real == "rem"){
+                    sleepstate = SleepState.REM
+                }
+
+
+                var rawData = SleepApiRawDataEntity(singledata.timestampSeconds, singledata.confidence, singledata.motion, singledata.light, sleepstate)
+
+
+                sleep1DbRepository.insertSleepApiRawData(rawData)
+            }
+
+            var sleepTime = SleepTimes(sleepTimeStart = startTime, sleepTimeEnd = endTimeTime)
+            var session = UserSleepSessionEntity(id = startTime, sleepTimes = sleepTime)
+            sleepDbRepository.insertUserSleepSession(session)
+
+        }
+
+
+
+        var a = 1
     }
 }
 
