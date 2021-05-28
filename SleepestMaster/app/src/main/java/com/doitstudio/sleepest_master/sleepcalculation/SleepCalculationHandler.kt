b82527c09c415198b745fec953d13c8cd3e148c1
Thomas Kreidl <vanthomiy@live.de>
2021-05-28 12:50:59 +0200

    Disabled light user wakeup

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/sleepcalculation/SleepCalculationHandler.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/sleepcalculation/SleepCalculationHandler.kt
@@ -375,10 +375,10 @@ class SleepCalculationHandler(val context: Context) {
 
             val actualTimeSeconds = getSecondsOfDay()
             var wakeUpTime = actualTimeSeconds + (restSleepTime)
-
+            var wakeUpTimeNew = 0
             // if in bed then check the single states of the sleep
             if (sleepSessionEntity.mobilePosition == MobilePosition.INBED) {
-                wakeUpTime = findLightUserWakeup(sleepApiRawDataEntity, wakeUpTime)
+                wakeUpTimeNew = findLightUserWakeup(sleepApiRawDataEntity, wakeUpTime)
             }
 
             // store in the alarm...!!!
