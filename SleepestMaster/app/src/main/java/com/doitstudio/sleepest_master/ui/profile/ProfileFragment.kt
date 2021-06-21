6f1fdd6527bbc9e04ee33a3d8da5106549db48d5
Thomas Kreidl <vanthomiy@live.de>
2021-06-21 08:03:41 +0200

    Added some layouts

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/profile/ProfileFragment.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/profile/ProfileFragment.kt
@@ -1,19 +1,28 @@
 package com.doitstudio.sleepest_master.ui.profile
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.doitstudio.sleepest_master.MainApplication
 import com.doitstudio.sleepest_master.R
 import com.doitstudio.sleepest_master.alarmclock.AlarmClockReceiver
+import com.doitstudio.sleepest_master.databinding.FragmentProfileBinding
+import com.doitstudio.sleepest_master.databinding.FragmentSleepBinding
 import com.doitstudio.sleepest_master.sleepcalculation.SleepCalculationHandler
+import com.doitstudio.sleepest_master.ui.sleep.SleepFragment
+import com.doitstudio.sleepest_master.ui.sleep.SleepViewModel
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import java.time.Instant
@@ -25,170 +34,35 @@ import java.util.*
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var profileViewModel: ProfileViewModel
+    private val viewModel by lazy { ViewModelProvider(this).get(ProfileViewModel::class.java)}
+    private lateinit var binding: FragmentProfileBinding
     private val actualContext: Context by lazy {requireActivity().applicationContext}
 
+
+    companion object {
+        fun newInstance() = SleepFragment()
+    }
+
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
+            inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        //region Test
-
-        var pref = actualContext.getSharedPreferences("AlarmChanged", 0)
-        val textAlarm = """
-            Last Alarm changed: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)},${pref.getInt(
-                "actualWakeup",
-                0
-        )},${pref.getInt("alarmUse", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("StartService", 0)
-        val textStartService = """
-            Last service start: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("StopService", 0)
-        val textStopService = """
-            Last service stop: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("Workmanager", 0)
-        val textLastWorkmanager = """
-            Last workmanager call: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("WorkmanagerCalculation", 0)
-        val textLastWorkmanagerCalculation = """
-            Last workmanagerCalc call: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("AlarmClock", 0)
-        val textCalc1 = """
-            Alarmclock: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("AlarmSet", 0)
-        val textCalc2 = """
-            AlarmSet: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)},${pref.getInt("hour1", 0)}:${pref.getInt("minute1", 0)},${
-            pref.getInt("actualWakeup", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("AlarmReceiver", 0)
-        val textAlarmReceiver = """
-            AlarmReceiver: ${pref.getInt("hour", 0)}:${pref.getInt("minute", 0)},${pref.getInt("intent", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("SleepTime", 0)
-        val textSleepTime= """
-            SleepTime: ${pref.getInt("sleeptime", 0)}
-            
-            """.trimIndent()
-        pref = actualContext.getSharedPreferences("StopException", 0)
-        val textStopException = """
-            Exc.: ${pref.getString("exception", "XX")}
-            
-            """.trimIndent()
-
-        var textGesamt = textAlarm + textStartService + textStopService + textLastWorkmanager + textLastWorkmanagerCalculation + textCalc1 + textCalc2 + textAlarmReceiver + textSleepTime + textStopException
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        viewModel.transitionsContainer = (binding.linearAnimationlayout)
+        viewModel.animatedTopView = binding.animatedTopView
+        binding.profileViewModel = viewModel
 
 
+        return binding.root
 
-        val text: TextView = root.findViewById(R.id.text_dashboard)
-        text.setText(textGesamt)
-
-        val btn : Button = root.findViewById(R.id.btnStartForegroundTest)
-        btn.setOnClickListener() {
-            //startOrStopForegroundService(Actions.START, actualContext);
-            /*val startForegroundIntent = Intent(context, ForegroundActivity::class.java)
-            startForegroundIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
-            startForegroundIntent.putExtra("intent", 1)
-            startActivity(startForegroundIntent)*/
-            val calendar = Calendar.getInstance()
-            //export();
-
-            AlarmClockReceiver.startAlarmManager(calendar.get(Calendar.DAY_OF_WEEK), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE) + 2, actualContext, 1)
-            //Toast.makeText(actualContext, "Gut gemacht, die App wird jetzt zerstört", Toast.LENGTH_LONG).show()
-        }
-
-        //endregion
-
-        return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    // region export
-
-    var sleepOutClassifyOutputExportBed = ""
-
-    private val repository by lazy { (actualContext as MainApplication).dataBaseRepository }
-
-    private fun dataPrep() = runBlocking{
-        val data = repository.allSleepApiRawData.first()
-        data.forEach {
-
-            val time = it.timestampSeconds.toLong() * 1000; // wokraround to change format
-
-            val instantNow = Instant.now()
-            val date = millisToDateTime(time)
-            sleepOutClassifyOutputExportBed += "${date.toLocalDate()};${date.hour}:${date.minute};${it.confidence};${(it.light)};${(it.motion)};0\n"
-            // Just display values that are shorter than 24Hours away
-        }
     }
-
-    fun export(){
-
-        val handler = SleepCalculationHandler(actualContext)
-
-        handler.defineUserWakeup()
-
-        /*dataPrep()
-
-        var switchExportFile =  "Datum;Uhrzeit;Schlaf;Licht;Bewegung;Wahre Zeiten"
-
-        val splitOut  = sleepOutClassifyOutputExportBed.split("\n")
-
-        splitOut.reversed().forEach {
-            switchExportFile += "${it}\n";
-        }
-
-        val shareIntent: Intent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, switchExportFile)
-            type = "text/csv"
-        }
-
-        startActivity(Intent.createChooser(shareIntent, "Export data"))*/
-    }
-
-    private fun millisToStringDateTime(millis: Long) : String {
-        // define once somewhere in order to reuse it
-        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-
-        // JVM representation of a millisecond epoch absolute instant
-        val instant = Instant.ofEpochMilli(millis)
-
-        // Adding the timezone information to be able to format it (change accordingly)
-        val date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())//.plusHours(1) //reudiger workaround haha
-        return formatter.format(date) // 10/12/2019 06:35:45
-    }
-
-    private fun millisToDateTime(millis: Long) : LocalDateTime {
-        // define once somewhere in order to reuse it
-        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-
-        // JVM representation of a millisecond epoch absolute instant
-        val instant = Instant.ofEpochMilli(millis)
-
-        // Adding the timezone information to be able to format it (change accordingly)
-        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault())//.plusHours(1) //reudiger workaround haha
-    }
-
-    //endregion
-
 }
 
 
--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/profile/ProfileViewModel.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,161 @@
 package com.doitstudio.sleepest_master.ui.profile
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.transition.TransitionManager
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.widget.NestedScrollView
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableField
+import androidx.lifecycle.AndroidViewModel
+import com.doitstudio.sleepest_master.MainApplication
+import com.doitstudio.sleepest_master.storage.DataStoreRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import java.time.LocalTime
+import kotlin.math.abs
 
-class ProfileViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+class ProfileViewModel(application: Application) : AndroidViewModel(application) {
+    private fun getStringXml(id: Int): String {
+        return getApplication<Application>().resources.getString(id)
     }
-    val text: LiveData<String> = _text
+
+    //region binding values
+
+
+    private val scope: CoroutineScope = MainScope()
+    private val context by lazy { getApplication<Application>().applicationContext }
+    private val dataStoreRepository: DataStoreRepository by lazy {
+        (context as MainApplication).dataStoreRepository
+    }
+
+    fun onAppStatusClick(view: View) {
+
+    }
+
+    fun onAppFunctionClick(view: View) {
+
+    }
+
+    fun onReportProblemClick(view: View) {
+
+    }
+
+    fun onRateAppClick(view: View) {
+
+    }
+
+    val darkMode = ObservableField(true)
+    fun darkModeToggled(view: View) {
+        scope.launch {
+            darkMode.get()?.let {
+                dataStoreRepository.updateAutoSleepTime(it)
+                darkMode.set(!it)
+            }
+        }
+    }
+
+    val designExpand = ObservableField(View.GONE)
+    val helpExpand = ObservableField(View.GONE)
+    val aboutUsExpand = ObservableField(View.GONE)
+    val permissionsExpand = ObservableField(View.GONE)
+    fun onExpandClicked(view: View) {
+        updateExpandChanged(view.tag.toString(), true)
+    }
+    private fun updateExpandChanged(value: String, toggle: Boolean = false) {
+
+        TransitionManager.beginDelayedTransition(transitionsContainer);
+
+        designExpand.set(if (value == "0" && designExpand.get() == View.GONE) View.VISIBLE else View.GONE)
+        helpExpand.set(if (value == "1" && helpExpand.get() == View.GONE) View.VISIBLE else View.GONE)
+        aboutUsExpand.set(if (value == "2" && aboutUsExpand.get() == View.GONE) View.VISIBLE else View.GONE)
+        permissionsExpand.set(if (value == "3" && permissionsExpand.get() == View.GONE) View.VISIBLE else View.GONE)
+    }
+
+    val languageSelections = ObservableArrayList<String>()
+    val selectedLanguage = ObservableField(0)
+    fun onLanguageChanged(
+            parent: AdapterView<*>?,
+            selectedItemView: View,
+            language: Int,
+            id: Long
+    ){
+        scope.launch {
+            dataStoreRepository.updateStandardMobilePosition(language)
+        }
+
+    }
+    //endregion
+
+    init {
+
+        scope.launch {
+            var sleepParams = dataStoreRepository.sleepParameterFlow.first()
+            val time = LocalTime.ofSecondOfDay(sleepParams.normalSleepTime.toLong())
+
+            languageSelections.addAll(arrayListOf<String>("Deutsch", "Englisch"))
+        }
+    }
+
+    //region animation
+
+    lateinit var transitionsContainer : ViewGroup
+    lateinit var transitionsContainerTop : ViewGroup
+    lateinit var animatedTopView : MotionLayout
+    lateinit var imageMoonView : AppCompatImageView
+
+    fun onShowTips(view: View){
+        animateTop(true)
+
+    }
+
+    var lastScroll = 0
+    var lastScrollDelta = 0
+    var progress = 0f
+    var newProgress = 0f
+    fun onScrollChanged(v: NestedScrollView, l: Int, t: Int, oldl: Int, oldt: Int) {
+        //Log.d(TAG, "scroll changed: " + this.getTop() + " "+t);
+        val scrollY: Int = v.scrollY // For ScrollView hprizontal use getScrollX()
+        val b = l
+        val c = t
+        val d  = oldl
+        //TransitionManager.beginDelayedTransition(transitionsContainerTop);
+
+        newProgress = (1f / 500f) * scrollY
+        animatedTopView.progress = newProgress
+
+        if(abs(progress - newProgress) > 0.25 ) {
+            progress = newProgress
+        }
+
+        lastScroll = scrollY
+    }
+
+    var lastMotionEvent : Int = MotionEvent.ACTION_UP
+
+
+    val pictureScale = ObservableField(1.0f)
+
+
+    private fun animateTop(expand: Boolean){
+
+
+        if(expand)
+        {
+            pictureScale.set(0.25f)
+        }
+        else
+        {
+            pictureScale.set(1f)
+        }
+    }
+
+
+    //endregion
 }
