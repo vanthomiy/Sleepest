93e2abef4b6d7751831e84fa56dc65943048a28a
Thomas Kreidl <vanthomiy@live.de>
2021-07-10 11:43:59 +0200

    Added some comments and removed the light user wakeup from the project
    Co-Authored-By: Tim Groß <60232880+Meraxa@users.noreply.github.com>

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/sleepcalculation/ml/ModelInputAssignment.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/sleepcalculation/ml/ModelInputAssignment.kt
@@ -2,14 +2,28 @@ package com.doitstudio.sleepest_master.sleepcalculation.ml
 
 
 /**
- * Helper class to map the input on the input of a model. The necessary json files are stored in the Assets
+ * Helper class to map the input on the input of a model.
+ * The necessary json files are stored in the assets/ml/InputAssignmentsXXX
+ * They are auto-created when creating a new ml model in the python code
  */
-data class ModelInputAssignment (
+data class ModelInputAssignment(
 
-        val shape : String,
-        val name : String,
-        val index : Int,
-        val dtype : String
+        /**
+         * The actual input shape that is expected by the model (should be [1 1])
+         */
+        val shape: String,
+        /**
+         * The name of the input shape: Its always serving_default_xxx where xxx is rather sleep, motion or brightness added by an index
+         */
+        val name: String,
+        /**
+         * The index of the actual input (Which input of the model)
+         */
+        val index: Int,
+        /**
+         * The data type of the input (Should always be int32)
+         */
+        val dtype: String
 
 )
 
