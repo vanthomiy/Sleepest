9f396b064558a425a13a3ff69df77bedd0d41ad4
Tim Groß <tim.lk@web.de>
2021-07-16 12:39:49 +0200

    Added dynamic date actualisation

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/history/HistoryTabView.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/history/HistoryTabView.kt
@@ -20,6 +20,8 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import java.time.LocalDate
 import java.time.Month
+import java.time.temporal.WeekFields
+import java.util.*
 
 class HistoryTabView : Fragment() {
     // When requested, this adapter returns a DemoObjectFragment,
@@ -57,9 +59,11 @@ class HistoryTabView : Fragment() {
         tVActualDayTabView = view.findViewById(R.id.tV_actualDayTabView)
         previousMonthAnalysisDate = LocalDate.now().month
 
+        val tabs = listOf("Day", "Week", "Month")
+
         val tabLayout = binding.tabLayout
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = "Day ${(position + 1)}"
+            tab.text = tabs[position]
         }.attach()
 
         btnPrevious.setOnClickListener {
@@ -85,11 +89,111 @@ class HistoryTabView : Fragment() {
 
                     previousMonthAnalysisDate = viewModel.analysisDate.get()?.month!!
 
-                    tVActualDayTabView.text = viewModel.analysisDate.get().toString()
+                    updateDateInformation(tabLayout.selectedTabPosition)
                 }
             }
         )
 
-        viewModel.analysisDate.set(LocalDate.now())
+        updateDateInformation(tabLayout.selectedTabPosition)
+    }
+
+    fun updateDateInformation(range: Int) {
+        when (range) {
+            0 -> tVActualDayTabView.text = createCalendarDayInformation()
+            1 -> tVActualDayTabView.text = createCalendarWeekInformation()
+            2 -> tVActualDayTabView.text = createCalendarMonthInformation()
+        }
+    }
+
+    private fun createCalendarDayInformation(): String {
+        val actualDay = LocalDate.now()
+        var information : String
+
+        viewModel.analysisDate.get()?.let {
+
+            information = when {
+                actualDay.dayOfYear == it.dayOfYear -> {
+                    "Today"
+                }
+                (actualDay.dayOfYear - 1) == it.dayOfYear -> {
+                    "Yesterday"
+                }
+                else -> {
+                    ("${it.dayOfMonth}.${it.monthValue}.${it.year}")
+                }
+            }
+        }
+
+        return information
+    }
+
+    private fun createCalendarWeekInformation(): String {
+        var analysisWeekOfYear: Int
+        val actualDate = LocalDate.now()
+        val actualWeekOfYear: Int
+        var information : String
+
+        val actualCalendar = Calendar.getInstance()
+        actualCalendar.set(actualDate.year, actualDate.monthValue, actualDate.dayOfMonth)
+        actualWeekOfYear = actualCalendar.get(Calendar.WEEK_OF_YEAR)
+
+        viewModel.analysisDate.get()?.let {
+            val analysisCalendar = Calendar.getInstance()
+            analysisCalendar.set(it.year, it.monthValue, it.dayOfMonth)
+            analysisWeekOfYear = analysisCalendar.get(Calendar.WEEK_OF_YEAR)
+
+            information = when {
+                actualWeekOfYear == analysisWeekOfYear -> {
+                    "This week"
+                }
+                (actualWeekOfYear - 1) == analysisWeekOfYear -> {
+                    "Last week"
+                }
+                else -> {
+                    getWeekRange(it)
+                }
+            }
+        }
+
+        return information
+    }
+
+    private fun getWeekRange(date: LocalDate) : String {
+        val sundayOfWeek : LocalDate = when (date.dayOfWeek.value) {
+            1 -> date.plusDays(6L) // Monday
+            2 -> date.plusDays(5L) // Tuesday
+            3 -> date.plusDays(4L) // Wednesday
+            4 -> date.plusDays(3L) // Thursday
+            5 -> date.plusDays(2L) // Friday
+            6 -> date.plusDays(1L) // Saturday
+            else -> date.plusDays(0L) // Sunday
+        }
+        val mondayOfWeek : LocalDate = sundayOfWeek.minusDays(6L)
+
+        val mondayString : String = mondayOfWeek.dayOfMonth.toString() + "." + mondayOfWeek.monthValue
+        val sundayString : String = sundayOfWeek.dayOfMonth.toString() + "." + sundayOfWeek.monthValue
+
+        return ("$mondayString - $sundayString")
+    }
+
+    private fun createCalendarMonthInformation(): String {
+        viewModel.analysisDate.get()?.let {
+            return when (it.monthValue) {
+                1 -> "January"
+                2 -> "February"
+                3 -> "March"
+                4 -> "April"
+                5 -> "Mai"
+                6 -> "June"
+                7 -> "July"
+                8 -> "August"
+                9 -> "September"
+                10 -> "October"
+                11 -> "November"
+                else -> "December"
+            }
+        }
+
+        return "none"
     }
 }
