e808eaa4fcc7760c6e2c802cc6a9e6dbc4344c0e
FabiM96 <Fabii9@gmx.de>
2021-09-03 16:23:55 +0200

    Test Autostart

--- SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/settings/SettingsFragment.kt
+++ SleepestMaster/app/src/main/java/com/doitstudio/sleepest_master/ui/settings/SettingsFragment.kt
@@ -44,6 +44,7 @@ import com.doitstudio.sleepest_master.util.NotificationUtil
 import com.doitstudio.sleepest_master.util.SmileySelectorUtil
 import com.doitstudio.sleepest_master.util.TimeConverterUtil
 import com.google.gson.Gson
+import com.judemanutd.autostarter.AutoStartPermissionHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.first
@@ -96,7 +97,8 @@ class SettingsFragment : Fragment() {
             onPermissionClicked(it)
         }
         binding.overlayPermission.setOnClickListener {
-            onPermissionClicked(it)
+            //onPermissionClicked(it)
+            AutoStartPermissionHelper.getInstance().getAutoStartPermission(actualContext, true, true)
         }
         binding.importButton.setOnClickListener {
             onDataClicked(it)
