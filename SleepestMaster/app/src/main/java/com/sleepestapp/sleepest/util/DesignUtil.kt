f60f8b59ab0de5b55a30db0fb2f9a545ea45a38b
Meraxa <60232880+Meraxa@users.noreply.github.com>
2021-10-09 10:47:05 +0200

    Update darkmode handling

--- SleepestMaster/app/src/main/java/com/sleepestapp/sleepest/util/DesignUtil.kt
+++ SleepestMaster/app/src/main/java/com/sleepestapp/sleepest/util/DesignUtil.kt
@@ -5,12 +5,12 @@ import android.content.res.Configuration
 import android.graphics.Color
 
 object DesignUtil {
-    fun checkDarkModeActive(context: Context) : Boolean {
-        return when (context.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
-            Configuration.UI_MODE_NIGHT_NO -> {
-                false
-            }
-            else -> true
+    fun checkDarkModeActive(context: Context, appDarkMode: Boolean, darkModeAuto: Boolean) : Boolean {
+        val systemDarkMode = context.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)
+        return if (darkModeAuto) {
+            systemDarkMode == Configuration.UI_MODE_NIGHT_YES
+        } else {
+            appDarkMode
         }
     }
 
